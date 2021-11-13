6e5300adeb24caf1ee144ea050a383d7d1ea2eb3
Joaco <rosariodota3@gmail.com>
2021-11-13 11:00:47 -0300

    auth uses email as username or generates one if not available

--- app/src/main/java/com/puntogris/recap/feature_auth/data/data_source/remote/FirebaseAuthApi.kt
+++ app/src/main/java/com/puntogris/recap/feature_auth/data/data_source/remote/FirebaseAuthApi.kt
@@ -1,7 +1,8 @@
 package com.puntogris.recap.feature_auth.data.data_source.remote
 
 import com.google.firebase.auth.AuthCredential
-import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.Transaction
 import com.puntogris.recap.core.data.remote.FirebaseClients
 import com.puntogris.recap.core.utils.Constants
 import com.puntogris.recap.core.utils.IDGenerator
@@ -10,32 +11,36 @@ import kotlinx.coroutines.tasks.await
 
 class FirebaseAuthApi(private val firebase: FirebaseClients) : AuthServerApi {
 
+    private val usersCollection = firebase.firestore.collection(Constants.USERS_COLLECTION)
+
     override suspend fun signInAndCreateUser(credential: AuthCredential) {
-
         val authResult = firebase.auth.signInWithCredential(credential).await()
-        val user = requireNotNull(authResult.user)
+        createUser(requireNotNull(authResult.user))
+    }
 
-        val publicRef = firebase
-            .firestore
-            .collection(Constants.USERS_COLLECTION)
-            .document(user.uid)
+    private suspend fun createUser(user: FirebaseUser){
+        val publicRef = usersCollection.document(user.uid)
 
         val privateRef = publicRef
             .collection(Constants.PRIVATE_PROFILE_COLLECTION)
             .document(user.uid)
 
-        if (!privateRef.get().await().exists()) {
-            val username = IDGenerator.randomID()
-            val usernameRef = firebase.firestore
-                .collection(Constants.USERNAMES_COLLECTION)
-                .document(username)
+        val usernameEmailRef = usersCollection.document(requireNotNull(user.email))
 
-            firebase.firestore.runBatch {
-                it.set(privateRef, user.toPrivate(username))
-                it.set(publicRef, user.toPublic(username))
-                it.set(usernameRef, Constants.UID_FIELD to user.uid)
-            }.await()
-        }
+        firebase.firestore.runTransaction {
+            if (it.get(publicRef).exists()) return@runTransaction
+
+            val username =
+                if (it.get(usernameEmailRef).exists()) {
+                    requireNotNull(user.email)
+                } else {
+                    IDGenerator.randomID()
+                }
+
+            it.set(usersCollection.document(username), Constants.UID_FIELD to user.uid)
+            it.set(privateRef, user.toPrivate(username))
+            it.set(publicRef, user.toPublic(username))
+        }.await()
     }
 
     override fun signOut() {
